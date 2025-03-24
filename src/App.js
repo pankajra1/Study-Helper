7aada357e586751f80e03b212f0dfa0e100cdeb6
pankajra1 <pankajrai1807@gmail.com>
2025-03-25 00:35:34 +0530

    app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import logo from './logo.png';
+// import logo from './logo.png';
 import './App.css';
 // Import Components
 import Home from './Home';
